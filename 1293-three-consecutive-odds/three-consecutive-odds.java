5541a2359932e6057010bf798289fcc05e74f589
Amrit Raj <cnamritraj9123@gmail.com>
2026-06-17 19:13:44 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (18.98%) - LeetSync

---
+++ 1293-three-consecutive-odds/three-consecutive-odds.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int n=arr.length;
+        if(n<3) return false;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]%2!=0){
+                ans++;
+                if(ans==3) return true;
+            }else{
+                ans=0;
+            }
+        }
+        return false;
+    }
+}
