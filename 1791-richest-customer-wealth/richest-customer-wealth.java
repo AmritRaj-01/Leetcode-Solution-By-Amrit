70f73ff435f2f027ae2e86b95a0492c875aa3750
Amrit Raj <cnamritraj9123@gmail.com>
2026-07-09 23:32:32 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (5.72%) - LeetSync

---
+++ 1791-richest-customer-wealth/richest-customer-wealth.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max=0;
+        for(int i=0;i<accounts.length;i++){
+            int currWealth=0;
+            for(int j=0;j<accounts[i].length;j++){
+                currWealth += accounts[i][j];
+            }
+            if(currWealth>max){
+                max=currWealth;
+            }
+        }
+        return max;
+    }
+}
