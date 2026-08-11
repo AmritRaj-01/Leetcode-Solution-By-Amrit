d680e0ea3e5c75a211eddedebe7cfbc476dd1b27
Amrit Raj <cnamritraj9123@gmail.com>
2026-08-11 06:50:35 +0530

    Time: 18 ms (40.12%) | Memory: 45.1 MB (74.42%) - LeetSync

---
+++ 437-path-sum-iii/path-sum-iii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int helper(TreeNode root,long sum){
+        if(root==null) return 0;
+        int count=0;
+        if((long)(root.val)==sum) count ++;  
+        return count + helper(root.left,sum-(long)(root.val))+helper(root.right,sum-(long)(root.val));
+
+    }
+    public int pathSum(TreeNode root, int targetSum) {
+        if(root==null) return 0;
+        int count= helper(root,(long)(targetSum));
+        count+=(pathSum(root.left,targetSum)+pathSum(root.right,targetSum));
+        return count;
+    }
+}
